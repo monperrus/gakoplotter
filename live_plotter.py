356730a7c55132a3d90cf39375dfab30ba676776
Martin Monperrus <martin.monperrus@gnieh.org>
2026-02-07 12:08:01 +0100

    init

---
+++ live_plotter.py
@@ -0,0 +1,271 @@
+#!/usr/bin/python3
+# live coding the pen plotter
+# usage: 
+# prerequisites:
+# /home/martin/bin/gcode-cli from https://github.com/hzeller/gcode-cli
+# first start the socket with 
+# nc -lU /tmp/tio-socket0 | gcode-cli -
+# python3
+# from live_plotter import *
+# init()
+# q(10)
+# l(x,y) # line to x,y
+#
+# # %load_ext autoreload
+# %autoreload 2
+
+
+#-------------------------------------------------------------------------------
+
+import tkinter as tk, tkinter.constants, tkinter.filedialog
+import re
+import PIL
+from PIL import ImageDraw, Image, ImageOps
+import math
+import os
+import sys
+import socket
+import importlib
+
+
+def gcode2dict(filename):
+    return gcode2dict_internal(open(filename,"r").readlines())
+
+def gcode2dict_internal(lines):
+    thisGcodeLine= {}
+    gCodeDict =[]
+    separator = '(M|G|X|Y|Z|I|J|K|F|S|P|;)'
+    regex = re.compile(separator,flags= re.IGNORECASE)
+    for line in lines:
+        thisGcodeLine= {}
+        lineList = regex.split(line.rstrip('\n ').upper())
+        for i in range(len(lineList)):
+            if lineList[i] == (";" or "(" or ""):
+                break
+            try:
+                if lineList[i] in separator: thisGcodeLine[lineList[i].upper()] = float(lineList[i+1].rstrip('\n '))
+            except:
+                pass
+        gCodeDict.append(thisGcodeLine)
+    #print(gCodeDict)
+    return gCodeDict
+
+def replace(filename, outputfilename, gCodeDict, GValue, axis, searchValue, newValue):
+    posX, posY,posZ,i,j,k,g = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0
+    a_list = ("M","G", "X", "Y", "Z", "I", "J", "K", "F", "S", "P")
+
+    if searchValue == "*":
+
+        for l in  gCodeDict:
+            if l.get('G', None) != None:
+                g = l.get('G')
+                l["G"] = int(l.get("G"))
+            if (l.get('G', None) == GValue) or (g == GValue) :
+                l[axis]= newValue
+
+
+        with open(outputfilename,"w") as f:
+
+            for l in  gCodeDict:
+                lsorted =[(key, l[key]) for key in a_list if key in l]
+                for key, value in lsorted:
+                    f.write(key + str(value) + " ")
+                f.write("\n")
+
+
+
+def dict2image(gCodeDict, draw, linewidth):
+    posX, posY,posZ,i,j,k,g = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0
+    return dict2image_internal(gCodeDict, draw, linewidth, posX, posY,posZ,i,j,k,g)
+
+def dict2image_internal(gCodeDict, draw, linewidth, posX, posY,posZ,i,j,k,g):
+    print(posX, posY,posZ,i,j,k,g)
+    #posX, posY,posZ,i,j,k,g = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0
+    scaler = 1.0
+    seqX = [x.get('X', 0) for x in gCodeDict]
+    seqI = [x.get('I', 0) for x in gCodeDict]
+    minX = min(seqX)
+    maxX = max(seqX)
+    seqY = [x.get('Y', 0) for x in gCodeDict]
+    seqJ = [x.get('J', 0) for x in gCodeDict]
+    minY = min(seqY)
+    maxY = max(seqY)
+    seqZ = [x.get('Z', 0) for x in gCodeDict]
+    minZ = min(seqZ)
+    maxZ = max(seqZ)
+    #offsetX = 0.0-minX
+    offsetX = linewidth
+    #offsetY = 0.0-minY
+    offsetY = linewidth
+    offsetZ = 0.0-minZ
+    dictScaler = ["X","Y","Z","I","J","K"]
+    colorZ =0
+    if maxX - minX == 0: maxX= 1e-200
+    if maxY - minY == 0: maxY= 1e-200
+    #scaler = min(sizeX/(maxX - minX),sizeY/(maxY - minY))
+    scaler = 1
+    
+    
+    ## setting scalerZcolor
+    if maxZ-minZ == 0: scalerZcolor = 1
+    elif maxZ-minZ > 0: scalerZcolor = 255/(maxZ-minZ)/scaler
+    else: scalerZcolor = 255/(minZ-maxZ)/scaler
+    print("scalerZcolor",scalerZcolor, maxZ,minZ)
+    scalerZcolor = 51
+
+    for l in  gCodeDict:
+        print("foo",l)
+        for key, value in l.items():
+            if key =="X":
+                l[key]= (value + offsetX)* scaler
+            elif key =="Y":
+                l[key]= (value + offsetY) * scaler
+            #elif key =="Z":
+                #l[key]= (value + offsetZ)* scaler
+            #elif key in dictScaler:
+                #l[key]= value * scaler
+        if l.get('X', None) != None or l.get('Y', None) != None or l.get('Z', None) != None or l.get('I', None) != None or l.get('J', None) != None or l.get('K', None) != None:
+            print(l)
+            if l.get('X', None) == None: l["X"]= posX
+            if l.get('Y', None) == None: l["Y"]= posY
+            if l.get('Z', None) == None: l["Z"]= posZ
+            if l.get('I', None) == None: l["I"]= i
+            if l.get('J', None) == None: l["J"]= j
+            if l.get('K', None) == None: l["K"]= k
+            if l.get('G', None) == None: l["G"]= g
+            colorZ = int(l.get("Z")*scalerZcolor)
+            if l.get('G', None) == 1:
+                print("draw line", [posX, posY,l.get('X'),l.get('Y'), colorZ, linewidth])
+                draw.line([posX, posY,l.get('X'),l.get('Y')], width=linewidth)
+            elif l.get('G', None) == 3:
+                centerX = posX+l.get("I")
+                centerY = posY+l.get("J")
+                if (posX-centerX) == 0:
+                    centerX+=1e-200
+                if (posY-centerY) == 0:
+                    centerY+=1e-200
+                startAngle = math.degrees(math.atan2((posY-centerY),(posX-centerX)))
+                if (l.get("X")-centerX) == 0:
+                    centerX+=1e-200
+                if (l.get("Y")-centerY) == 0:
+                    centerY+=1e-200
+                stopAngle = math.degrees(math.atan2((l.get("Y")-centerY),(l.get("X")-centerX)))
+                radius = math.sqrt(l.get("I")**2+l.get("J")**2)
+                if startAngle == stopAngle:
+                    stopAngle += 360
+                draw.arc([centerX-radius, centerY-radius, centerX+radius, centerY+radius], startAngle, stopAngle, fill=colorZ, width=linewidth)
+            elif l.get('G', None) == 2:
+                centerX = posX+l.get("I")
+                centerY = posY+l.get("J")
+                if (posX-centerX) == 0:
+                    centerX+=1e-200
+                startAngle = math.degrees(math.atan2((posY-centerY),(posX-centerX)))
+                if (l.get("X")-centerX) == 0:
+                    centerX+=1e-200
+                stopAngle = math.degrees(math.atan2((l.get("Y")-centerY),(l.get("X")-centerX)))
+                if startAngle == stopAngle: stopAngle += 360
+                radius = math.sqrt(l.get("I")**2+l.get("J")**2)
+                draw.arc([centerX-radius, centerY-radius, centerX+radius, centerY+radius], stopAngle, startAngle, fill=colorZ, width=linewidth)
+
+            if l.get('X', None) != None: posX = l.get('X')
+            if l.get('Y', None) != None: posY = l.get('Y')
+            if l.get('Z', None) != None: posZ = l.get('Z')
+            if l.get('I', None) != None: i= l.get("I")
+            if l.get('J', None) != None: j= l.get("J")
+            if l.get('K', None) != None: k= l.get("K")
+            if l.get('G', None) != None: g = l.get('G')
+    return posX, posY,posZ,i,j,k,g
+
+def init():
+    global draw,linewidth,image,state
+    sizeX = 300
+    sizeY = 420
+    linewidth = 5
+
+    image = Image.new(mode = "L", size = (sizeX, sizeY),color="white")
+    
+    state= ( 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0)
+
+    draw = ImageDraw.Draw(image)
+    d_internal(["G21", "G17", "G90", "F2000", "G00 X0 Y0", "G00 Z0"])
+
+
+try:
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    s.connect("/tmp/tio-socket0")
+except: pass
+
+def d_internal(ls): 
+    global draw,linewidth,image, state
+    print("state",state)
+    posX, posY,posZ,i,j,k,g = state
+    state = dict2image_internal(gcode2dict_internal(ls),draw, linewidth, posX, posY,posZ,i,j,k,g)
+    #image.show()
+    
+    # send to socket
+    # test with nc -lU /tmp/tio-socket0
+    # tio --socket unix:/tmp/tio-socket0 /dev/ttyACM0
+    try:
+        s.send(("\n".join(ls)+"\n").encode("utf-8"))
+    except Exception as e:
+        print(e)
+        
+
+def d(s):
+    d_internal([s])
+    
+
+def q(size):
+    global state
+    posX, posY,posZ,i,j,k,g = state
+    d_internal([
+        "G00 X"+str(posX)+ " Y"+str(posY),
+        "G0 Z5", 
+        "G01 X"+str(posX+size)+ " Y"+str(posY),
+        "G01 X"+str(posX+size)+ " Y"+str(posY+size),
+        "G01 X"+str(posX)+ " Y"+str(posY+size),
+        "G01 X"+str(posX)+ " Y"+str(posY),
+        "G0 Z0"
+        ])
+
+def m(x,y):
+    d_internal(["G0 Z0", "G00 X"+str(x)+ " Y"+str(y)])
+
+def l(x,y):
+    d_internal(["G0 Z5", "G01 X"+str(x)+ " Y"+str(y), "G0 Z0"])
+
+def r():
+    global live_plotter
+    """ reload the module """
+    importlib.reload(live_plotter)
+    
+##    return image
+def plotFile(filename):
+    #return
+    sizeX = 300
+    sizeY = 420
+    linewidth = 5
+
+    image = Image.new(mode = "L", size = (sizeX, sizeY),color="white")
+    draw = ImageDraw.Draw(image)
+
+
+    #filename = tkinter.filedialog.askopenfilename(initialdir = "./",title = "Select file",
+        #filetypes = (("nc-Files",".nc .cnc"),("all files","*.*")));
+    #root.destroy()
+    if filename != "":
+        with open(filename, 'r') as file:
+            input_line_count = sum(1 for line in file)
+        print(input_line_count)
+
+        gdict = gcode2dict(filename)
+        #print(gdict)
+        dict2image(gcode2dict(filename),draw, linewidth)
+        #dict2image(gcode2dict("plottablefieldlayer1.gcode"),draw, linewidth)
+        image = image.transpose(PIL.Image.FLIP_TOP_BOTTOM)
+        image.show()
+
+def foo():
+    print("s")
+if __name__ == '__main__':
+    plotFile(sys.argv[1])
---
+++ typewrite.py
@@ -0,0 +1,274 @@
+#!/bin/bash
+"""
+penplotter as typewriter
+
+uses g-code parsing
+
+usage: python -u typewrite.py | gcode-cli -
+
+
+could also be done using ./hf2gcode --no-post --no-pre "a" directly
+
+sophistication
+font size change
+different font change
+generative font
+
+"""
+from gcodeparser import GcodeParser
+import sys, tty, termios
+import random
+import os
+
+# echo cache/*.svg.*
+# I have only two fonts for now
+FONT="HersheyScript1"
+FONT="EMSOsmotron"
+
+SCALE=1
+
+def width_letter(l):
+  lfile = 'cache/'+FONT+'.svg.d/'+l+'.gcode'
+  if not os.path.exists(lfile): 
+    return 0
+  with open(lfile, 'r') as f:
+    gcode = f.read()
+  data = GcodeParser(gcode)
+  return max(x.params["X"] if "X" in x.params else 0 for x in GcodeParser(gcode).lines )
+
+Z=6.55
+def letter(l,translation_x,translation_y=0):
+  lfile = 'cache/'+FONT+'.svg.d/'+l+'.gcode'
+  if not os.path.exists(lfile): 
+    print("; letter not supported: "+l)
+    return
+  with open(lfile, 'r') as f:
+    gcode = f.read()
+    # hack to adjust height
+    gcode = gcode.replace("G0Z6.3","G0Z"+str(Z))
+  data = GcodeParser(gcode)
+  for x in data.lines:
+    #print(x.params)
+    if "X" in x.params:
+      x.params["X"]*=SCALE
+      x.params["X"]+=translation_x
+    if "Y" in x.params:
+      #print("foo")
+      x.params["Y"]*=SCALE
+      x.params["Y"]+=translation_y
+  return "\n".join(x.gcode_str for x in data.lines)
+
+def generativeletter(l,translation_x,translation_y=0):
+  with open('cache/'+FONT+'.svg.d/'+l+'.gcode', 'r') as f:
+    gcode = f.read()
+  data = GcodeParser(gcode)
+  for x in data.lines:
+    #print(x.params)
+    if "X" in x.params:
+      x.params["X"]+=translation_x+random.randint(0,1)
+    if "Y" in x.params:
+      #print("foo")
+      x.params["Y"]+=translation_y+random.randint(0,1)
+  return "\n".join(x.gcode_str for x in data.lines)
+
+
+def martin():
+  x=0
+  for i in "martin":
+    print(letter(i,x))
+    x+=10
+
+# https://stackoverflow.com/questions/510357/how-to-read-a-single-character-from-the-user
+def readchr():
+  fd = sys.stdin.fileno()
+  old_settings = termios.tcgetattr(fd)
+  try:
+      tty.setraw(sys.stdin.fileno())
+      ch = sys.stdin.read(1)
+  finally:
+      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+  return ch
+
+TOP=380
+x=0
+y=0
+
+print("G21 ; Set Units to Millimeters")
+print("G17 ; Set Plane Selection to XY")
+print("G90 ; Set Absolute Positioning")
+print("F5000  ; Set Speed to 2500 mm/min")
+
+
+NEWLINE_SPACE=14
+def newline():
+  global x,y
+  #pass
+#elif character=="\n":
+  print("; newline")
+  x=0
+  y-=NEWLINE_SPACE*SCALE
+  print("G00 X"+str(x)+"Y"+str(y))
+  
+def space():
+  global x,y
+  x+=10*SCALE
+  print("G00 X"+str(x))
+
+def randomspace(mins=0,maxs=10):
+  global x,y
+  x+=random.randint(mins,maxs)*SCALE
+  print("G00 X"+str(x))
+
+def print_char(character):
+  print("; "+str(ord(character)))
+  global x,y
+  if character=="\r" or character=="\n" :
+    newline()
+  elif character==" ":
+    space()
+  # elif ord(character)==126:    
+  elif character=="²":    
+    # print(";F5")
+    goto_origin()
+  elif character=="~":    
+    # print(";F5")
+    goto_top()
+  else:
+    print("; ",character)
+    print(letter(character,x,y))
+    #x+=10
+    x+=width_letter(character)*SCALE
+
+def goto_origin():
+  global x,y
+  print("G00 X0 Y0 ; F5 goto 0,0")
+  x=0
+  y=0
+
+def goto_top():
+  global x,y
+  y=TOP
+  print("G00 X"+str(x)+"Y"+str(y))
+
+def goto(_x,_y):
+  global x,y
+  x=_x
+  y=_y
+  print("G00 X"+str(_x)+"Y"+str(_y))
+  
+def typewrite_live():
+  global x,y
+  # we start at the opt left corner
+  # print("G00 X"+str(x)+"Y"+str(y))
+  goto_top()
+
+  for i in range(0,100000):
+    character = readchr()
+    print_char(character)
+
+
+def print_word(word):
+  for x in word:
+    print_char(x)
+
+def test1():
+  goto_top()
+  # goto_origin()
+  print_word("\n\n\nmartin")
+  goto_origin()
+
+def test2():
+  global NEWLINE_SPACE,x,y
+  NEWLINE_SPACE=3
+  INIT=int(TOP*1.0)
+  goto(0,INIT)
+  for i in range(0,10):  
+    print_char("\n")
+  # print_char("\n")
+  # print_char("\n")
+  # print_char("\n")
+  # goto_origin()
+  l=[x for x in "abcdefghjklmnopqrstuvwxyz"]
+  MAX=130
+  while y>INIT-(MAX*1.3):
+    while x<MAX:
+      print_char(random.choice(l))
+    newline()
+      
+  # print_word("\n\n\nmartin")
+  goto_origin()
+
+
+
+
+
+
+
+
+
+
+
+def test3():
+  """
+    in this one, I add my name and the current date in the middle
+
+  """
+  global NEWLINE_SPACE,x,y
+  global Z
+  # with and without the cardboard it's .75 difference
+  Z=5.89
+  #+.75
+  NEWLINE_SPACE=5.1
+  ## positioning at the beginning
+  y=380
+  print("G92 X0 Y"+str(y))
+
+  INIT=int(TOP*.8)
+  # goto(0,INIT)
+  l=[x for x in "abcdefghjklmnopqrstuvwxyz"]
+  MAX=130
+  linenumber=0
+  INCREASE=.02
+  while y>INIT-(MAX*.18): # postcard ratio is 3:2
+    charnumber = 0
+    where_random = random.randint(3,10)
+    while x<MAX:
+      if linenumber==3 and charnumber == 0:
+        Z+=INCREASE
+        # print_word("martin")
+        Z-=INCREASE
+      if linenumber==8 and charnumber == 0:
+        Z+=INCREASE
+        # print_word("monperrus")
+        Z-=INCREASE
+      if linenumber==13 and charnumber == 0:
+        Z+=INCREASE
+        print_word("18/01/2025")
+        Z-=INCREASE
+      if charnumber%2==1:
+        print_char(random.choice(l))
+      else: 
+        randomspace(3)
+        print_char('/')
+      charnumber +=1
+    newline()
+    linenumber+=1
+  print("; "+str(linenumber))
+  # print_word("\n\n\nmartin")
+  # goto_origin()
+
+def test_calibrate():
+  global Z
+  goto_top()
+  for x in range(30,60,1):
+    Z=str(x/10)
+    print_word(Z)
+    newline()
+  goto_origin()
+
+if __name__ == "__main__":
+  typewrite_live()
+  # test3()
+
+# test_calibrate()
+# typewrite_live()
